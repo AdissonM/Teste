01480aa7f1b0bd37728284a132c9a541a9ab3008
unknown <ADISSON6174@DIGITALCOLLEGE.LOCAL>
2024-11-12 15:01:11 -0300

    Teste

---
+++ Dropfiltro/src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+ <div className="container">
+  <div className="row">
+    <div className="col teste">
+      teste
+    </div>
+    <div className="col"></div>
+  </div>
+ </div>
+  
+
+
+    </>
+  )
+}
+
+export default App
